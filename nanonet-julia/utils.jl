96e592395ee2b766adf0895cc109321aba40787b
Simon Schneemelcher <schneemelchercom@gmail.com>
2022-11-19 20:18:17 +0100

    improve one_hot

--- nanonet-julia/utils.jl
+++ nanonet-julia/utils.jl
@@ -1,5 +1,5 @@
 function one_hot(y) 
-    y_new = zeros((size(y)[1], maximum(y) + 1)) 
-    [y_new[a, b] = 1 for (a, b) in zip(collect(1:size(y)[1]), y[:] .+ 1)]
+    y_new = zeros((length(y), maximum(y) + 1)) 
+    [y_new[a, b] = 1 for (a, b) in zip(1:length(y), y .+ 1)]
     return y_new
 end
