ccdfbf792e74f0cea3d07f5a5129f7cc7ed540cd
Simon Schneemelcher <schneemelchercom@gmail.com>
2022-11-19 13:55:42 +0100

    work on julia files

--- julia-example.jl
+++ julia-example.jl
@@ -4,15 +4,15 @@ include("nanonet-julia/model.jl")
 
 using MLDatasets
 
-x_train, y_train = MNIST.traindata(Float64, 1:10000)
+x_train = MNIST(:train).features #.traindata(Float64, 1:10000)
 x_train = reshape(permutedims(x_train, [3, 1, 2]), (length(x_train[1, 1, :]), 784))
 
 num_classes = 10
 
-layers = ((("input_shape", length(x_train[1, :])), ("units", 200), ("activation", "relu")),
-    (("units", 16), ("activation", "relu")),
-    (("units", 16), ("activation", "relu")),
-    (("units", num_classes), ("activation", "softmax")))
+layers = [Dict("input_shape" => length(x_train[1, :]), "units" => 200, "activation" => "relu"),
+    Dict("units" => 16, "activation" => "relu"),
+    Dict("units" => 16, "activation" => "relu"),
+    Dict("units" => num_classes, "activation" => "softmax")]
 
 model = build_model(layers)
 
--- nanonet-julia/model.jl
+++ nanonet-julia/model.jl
@@ -1,16 +1,34 @@
+using Base: tail
+
 include("activations.jl")
 
-function build_model(layers)
-    layers = map(Dict, layers)
-    model = []
-    for i in collect(1:length(layers))
-        layer = [randn((haskey(layers[i], "input_shape")
-                        ? get(layers[i], "input_shape", 16)
-                        : get(layers[i-1], "units", 16), get(layers[i], "units", 16))), get(layers[i], "activation", "relu")]
-        model = push!(model, layer)
-    end
-    return model
+function reduce_layer(acc, curr)
+    # display(summary(get(last(acc), "weights", [])))
+    input_shape = size(get(last(acc), "weights", [])[1])[2]
+    output_shape = get(curr, "units", 1)
+    push!(acc, Dict("weights" => [randn((input_shape, output_shape)), randn((1, output_shape))], "activation" => get(curr, "activation", "relu")))
 end
 
-f(acc, curr) = acc * curr[1] |> get(activation_map, curr[2], identity)
+function build_model(layers)
+    input_shape = get(first(layers), "input_shape", 1)
+    output_shape = get(first(layers), "units", 1)
+    input_layer = Dict("weights" => [
+            randn((input_shape, output_shape)), randn((1, output_shape))
+        ], "activation" => "relu")
+    reduce(reduce_layer, layers[2:length(layers)]; init=[input_layer])
+end
+
+#function build_model(layers)
+#    layers = map(Dict, layers)
+#    model = []
+#    for i in collect(1:length(layers))
+#        layer = [randn((haskey(layers[i], "input_shape")
+#                        ? get(layers[i], "input_shape", 16)
+#                        : get(layers[i-1], "units", 16), get(layers[i], "units", 16))), get(layers[i], "activation", "relu")]
+#        model = push!(model, layer)
+#    end
+#    return model
+#end
+
+f(acc, curr) = acc * get(curr, "weights", [])[1] .+ get(curr, "weights", [])[2] |> get(activation_map, get(curr, "activation", "relu"), relu)
 predict(model, x) = reduce(f, model; init=x)
